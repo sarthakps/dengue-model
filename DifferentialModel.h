2806a7650d5908c2d933fd7144ea81c47797d35b
Sarthak <sart@example.com>
2020-06-13 19:16:46 +0530

    Updated for length=128 data. Also added function for exporting matlab file.

--- DifferentialModel.h
+++ DifferentialModel.h
@@ -2,19 +2,25 @@ using namespace std;
 
 void differentialModel() {
     
+    double A = 13;          //constant recruitment rate of vector population
+    double totalCases = 0;
+
     //initial values
-    double Sv0 = 10;        // Initial No. of Susceptible Vector
+    double Sv0 = 8;        // Initial No. of Susceptible Vector
     double Sh0 = 1600;      // Initial No. of Susceptible Host
     double Iv0 = 1;         // Initial No. of Infected Vector
     double Ih0 = 0;         // Initial No. of Infected Host
     double R0 = 0;          // Initial No. of Recovered Patients
 
     //rates
-    double Dv = 1/12;       // Death Rate of Vector
+    double Dv = 1.0/12;       // Death Rate of Vector
     double Dh = 0;          // Death Rate of Host
-    double Bv = 0.000045;   // Transmission Rate Vector
-    double Bh = 0.0028;     // Transmission Rate Host
+    double Bv = 0.000045;   // Transmission Rate Host->Vector
+    double Bh = 0.00097;     // Transmission Rate Vector->Host
     double r = 0.03;        // Recovery Rate of Patients
+
+
+    cout<<endl<<endl<<Dv<<endl<<endl;
 
     //DIFFERENTIAL EQUATIONS
     // dSh/dt = -BhShIv;
@@ -23,18 +29,20 @@ void differentialModel() {
     // dIv/dt = BvSvIh - dvIv;
 
     dt = 1;
-    lengthOfSeason = 45;
+    lengthOfSeason = 80;
     
     vector<double> Sh;
     vector<double> Ih;
     vector<double> R;
     vector<double> Iv;
+    vector<double> Sv;
 
     //pushing initial values into vector containers
     Sh.push_back(Sh0);
     Ih.push_back(Ih0);
     R.push_back(R0);
     Iv.push_back(Iv0);
+    Sv.push_back(Sv0);
 
     cout<<endl<<endl;
 
@@ -43,6 +51,7 @@ void differentialModel() {
         double prev_Ih = Ih.back();
         double prev_R = R.back();
         double prev_Iv = Iv.back();
+        double prev_Sv = Sv.back();
 
         // dSh/dt = -BhShIv;
         // (next_Sh - prev_Sh) / dt = - Bh0 * prev_Sh * prev_Iv;
@@ -62,14 +71,23 @@ void differentialModel() {
         double next_R = prev_R + r * prev_Ih * dt;
         R.push_back(next_R);
 
+        double next_Sv = prev_Sv + (A - Bv*prev_Sv*prev_Ih - Dv*prev_Sv) * dt;
+        Sv.push_back(next_Sv);
+
         // dIv/dt = BvSvIh - DvIv;
         // (next_Iv - prev_Iv) / dt = Bv0 * Sv0 * prev_Ih - Dv0 * prev_Iv;
         // next_Iv = prev_IV + (Bv0 * Sv0 * prev_Ih - Dv0 * prev_Iv) * dt;
-        double next_Iv = prev_Iv + (Bv * Sv0 * prev_Ih - Dv * prev_Iv) * dt;
+        double next_Iv = prev_Iv + (Bv * prev_Sv * prev_Ih - Dv * prev_Iv) * dt;
         Iv.push_back(next_Iv);
 
         cout << "Day-" << day << "\t\t";
-        cout << "Sh: " << Sh.back() << "\t" << "Ih: " << Ih.back() << "\t" << "Rh: " << R.back() << "\t" << "Iv: " << Iv.back() <<endl;
+        // cout << "Sh: " << Sh.back() << "\t" << "Ih: " << Ih.back() << "\t" << "Rh: " << R.back() << "\t" << "Iv: " << Iv.back() <<endl;
+        cout << "Sh: " << Sh.back() << "\t" << ": " << Ih.back()<< "\t" 
+        << "Rh: " << (R.back()-R[day-1]) << "\t" << "Iv: " << Iv.back() << "\t" << "Sv: " << Sv.back() << endl;
+
+        totalCases += Ih.back()-Ih[day-1] + R.back()-R[day-1];
     }
+
+    cout<< endl << "Total Cases = " << totalCases << endl << endl;
      
 }
